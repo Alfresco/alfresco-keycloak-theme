4e3a282b1c557f7982cf52408149253d70adc0e0
Jamal Kaabi-Mofrad <jamalkm@users.noreply.github.com>
2020-04-14 13:40:02 +0100

    Switch to OAuth token.

--- release.sh
+++ release.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 # Available parameters:
-# - username
-# - password
+# - token
 # - prerelease
 #
 # Example:
-# release.sh username=johndoe password=pass prerelease=true
+# release.sh token=0123456 prerelease=true
 #
 
 set -o errexit
@@ -31,7 +30,7 @@ done
 REPO_URL="https://api.github.com/repos/Alfresco/alfresco-keycloak-theme"
 RELEASES_URL="$REPO_URL/releases"
 TAGS_URL="$REPO_URL/releases/tags/$THEME_VERSION"
-AUTH="$username:$password"
+TOKEN="$token"
 PRE_RELEASE="${prerelease:-false}"
 DISTRIBUTION_NAME="alfresco-keycloak-theme-$THEME_VERSION.zip"
 
@@ -43,13 +42,13 @@ log_info "Tag the current branch as $THEME_VERSION"
 git tag "$THEME_VERSION"
 
 # check credentials
-STATUS_CODE=$(curl -s -o /dev/null -w "%{http_code}" $REPO_URL -u "$AUTH")
+STATUS_CODE=$(curl -s -o /dev/null -w "%{http_code}" $REPO_URL -H "Authorization: token $TOKEN")
 if [ $STATUS_CODE -eq "401" ] || [ $STATUS_CODE -eq "404" ]; then
     log_error "Bad credentials. Status Code: $STATUS_CODE"
 fi
 
 log_info "Create $THEME_VERSION release..."
-STATUS_CODE=$(curl -s -o /dev/null -w "%{http_code}" "$RELEASES_URL" -u "$AUTH" \
+STATUS_CODE=$(curl -s -o /dev/null -w "%{http_code}" "$RELEASES_URL" -H "Authorization: token $TOKEN" \
     -H 'Content-Type: application/json' \
     -d "{
   \"tag_name\": \"$THEME_VERSION\",
@@ -68,7 +67,7 @@ WAIT_COUNTER=0
 WAIT_COUNTER_MAX=10
 WAIT_SLEEP_SECONDS=2
 while [ "$WAIT_COUNTER" -lt "$WAIT_COUNTER_MAX" ]; do
-    STATUS_CODE=$(curl -s -o /dev/null -w "%{http_code}" "$TAGS_URL" -u "$AUTH")
+    STATUS_CODE=$(curl -s -o /dev/null -w "%{http_code}" "$TAGS_URL" -H "Authorization: token $TOKEN")
     if [ $STATUS_CODE -eq "200" ]; then
         log_info "$THEME_VERSION release has been published."
         break
@@ -81,7 +80,7 @@ done
 
 # Get upload_url
 log_info "Getting upload asset URL from: $TAGS_URL"
-UPLOAD_URL=$(curl -s "$TAGS_URL" -u "$AUTH" | grep upload_url | sed 's/"upload_url":// ; s/"//g' | cut -d'{' -f 1 | tr -d '[:space:]')
+UPLOAD_URL=$(curl -s "$TAGS_URL" -H "Authorization: token $TOKEN" | grep upload_url | sed 's/"upload_url":// ; s/"//g' | cut -d'{' -f 1 | tr -d '[:space:]')
 
 log_info "Upload asset URL is: '$UPLOAD_URL'"
 
@@ -94,4 +93,4 @@ log_info "Uploading '$DISTRIBUTION_NAME' asset... "
 # Add asset name to the URL
 UPLOAD_ASSET_URL="$UPLOAD_URL?name=$(basename $DISTRIBUTION_NAME)"
 # Upload asset
-curl $UPLOAD_ASSET_URL -u "$AUTH" -H "Content-Type: application/octet-stream" --data-binary @"$DISTRIBUTION_NAME"
+curl $UPLOAD_ASSET_URL -H "Authorization: token $TOKEN" -H "Content-Type: application/octet-stream" --data-binary @"$DISTRIBUTION_NAME"
